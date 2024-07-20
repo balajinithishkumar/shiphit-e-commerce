e1c4419056b145d054b85ac60ede141e12011d74
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-07-20 12:20:03 +0530

    google sheet url is updated!

--- shiphit-Tracking-Feature/src/keys.js
+++ shiphit-Tracking-Feature/src/keys.js
@@ -1,5 +1,4 @@
 const  keys = {apikey: "asat_4719be070c7e4615a2a5a4414283acd1",
- shiphit_url :"https://script.google.com/macros/s/AKfycbyGrvyiJEa6ZF3qP-NSkGCXYuD4QT34zBkF86xb6hqdOC54VrY1w89R9QSaAaYGqbeP/exec"
+ shiphit_url :"https://script.google.com/macros/s/AKfycby5FtZ9zjHp11dLzx4SQXGkJoEmKHFWbntp_yfOZOJdZuxxqbuIvpWM017pPs9jg-dK/exec"
 }
 export default keys
-
